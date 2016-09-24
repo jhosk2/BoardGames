0ddae3f0c2c7fb6a694a362dbe9ac1d376d32621
조원빈 <podothecat@gmail.com>
2016-09-24 14:26:14 +0900

    core login 뷰 추가

--- core/models.py
+++ core/models.py
@@ -1,10 +1,3 @@
 from django.db import models
-from django.utils import timezone
 
-class Post(models.Model):
-    name = models.CharField(max_length=200)
-    created_date = models.DateTimeField(
-            default=timezone.now)
-
-    def __str__(self):
-        return self.name
+# Create your models here.
